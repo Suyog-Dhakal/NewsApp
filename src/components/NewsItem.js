5bd2267ccb0c993f52431ac583c77de84f57330e
Suyog-Dhakal <075bct092.suyog@pcampus.edu.np>
2021-11-28 20:38:01 +0545

    news fetched from newsapi

--- src/components/NewsItem.js
+++ src/components/NewsItem.js
@@ -10,7 +10,11 @@ export class NewsItem extends Component {
           <div className="card-body">
             <h5 className="card-title">{title}</h5>
             <p className="card-text">{description}</p>
-            <a href="/newsdetail" className="btn btn-sm btn-primary">
+            <a
+              href={newsUrl}
+              target="_blank"
+              className="btn btn-sm btn-primary"
+            >
               Read more
             </a>
           </div>
